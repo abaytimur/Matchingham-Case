5cee7e8db7502b8deff9fd7832d2ffe10321961c
Kadir Baytimur <a_baytimur@hotmail.com>
2023-04-11 03:26:33 +0300

    Rotation.

--- Assets/Scripts/Components/MatchItem/MatchItem.cs
+++ Assets/Scripts/Components/MatchItem/MatchItem.cs
@@ -1,4 +1,5 @@
 using Abstract;
+using DG.Tweening;
 using QuickOutline.Scripts;
 using UnityEngine;
 
@@ -10,9 +11,30 @@ namespace Components.MatchItem
         [field: SerializeField] public string ItemName { get; private set; }
         [SerializeField] private new Rigidbody rigidbody;
         [SerializeField] private Outline outline;
+        [SerializeField] private float rotationSpeed = .2f;
+        [SerializeField] private int rotationLoops = -1;
 
         public void SetRigidbody(bool isKinematic) => rigidbody.isKinematic = isKinematic;
-        public void OnHit(bool rayEnter) => outline.OutlineWidth = rayEnter ? 4f : 0f;
+
+        public void OnHit(bool rayEnter)
+        {
+            if (rayEnter)
+            {
+                transform.DORotate(new Vector3(45.0f, 0.0f, 0.0f), .3f).OnComplete(() =>
+                {
+                    transform
+                        .DORotate(new Vector3(0.0f, 360.0f, 0.0f), rotationSpeed, RotateMode.LocalAxisAdd).SetLoops(rotationLoops).SetEase(Ease.Linear);
+                });
+
+                outline.OutlineWidth = 4f;
+            }
+            else
+            {
+                transform.DOKill();
+                outline.OutlineWidth = 0f;
+            }
+        }
+
         private void Reset()
         {
             ItemName = name;
